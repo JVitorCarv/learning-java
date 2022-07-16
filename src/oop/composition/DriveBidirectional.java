837823197fd74e27b21e3eb76b0928616dbbcc2c
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-07-16 17:48:33 -0300

    Add bidirectional version of car and motor

--- src/oop/composition/DriveBidirectional.java
+++ src/oop/composition/DriveBidirectional.java
@@ -1,4 +1,26 @@
 package oop.composition;
 
 public class DriveBidirectional {
+    public static void main(String[] args) {
+        CarBidirectional car = new CarBidirectional();
+        System.out.println(car.isOn());
+
+        car.turnOn();
+        System.out.println(car.isOn());
+
+        System.out.println(car.motor.spins());
+
+        for (int i = 0; i < 4; i++) {
+            car.accelerate();
+        }
+        System.out.println(car.motor.spins());
+
+        for (int i = 0; i < 5; i++) {
+            car.brake();
+        }
+        System.out.println(car.motor.spins());
+
+        // Avoid bidirectionals!
+        System.out.println(car.motor.car.motor.car.motor.spins());
+    }
 }
--- src/oop/composition/MotorBidirectional.java
+++ src/oop/composition/MotorBidirectional.java
@@ -1,4 +1,19 @@
 package oop.composition;
 
 public class MotorBidirectional {
+     boolean isOn = false;
+     double injectionFactor = 1;
+
+     // Yes, this does not make a lot of sense...
+     CarBidirectional car;
+     MotorBidirectional(CarBidirectional car) {
+         this.car = car;
+     }
+
+     int spins() {
+         if (!isOn) {
+             return 0;
+         }
+         return (int) Math.round(injectionFactor *  3000);
+     }
 }
