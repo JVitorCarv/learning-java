d63cfef5a7e7035f999b19641e063229f7ff1c89
Vitor <vitor.velozo2003@gmail.com>
2022-07-27 18:00:24 -0300

    Add list/iterator/stream

---
+++ src/streams/PrintingObjects.java
@@ -0,0 +1,27 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PrintingObjects {
+    public static void main(String[] args) {
+        List<String> approved = Arrays.asList("Martin", "Kent", "Robert");
+
+        System.out.println("Using foreach...");
+        for (String name: approved) {
+            System.out.println(name);
+        }
+
+        System.out.println("\nUsing iterator...");
+        Iterator<String> iterator = approved.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\nUsing stream...");
+        Stream<String> stream = approved.stream();
+        stream.forEach(System.out::println);
+    }
+}
